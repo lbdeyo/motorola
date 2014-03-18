7d0c4cd09f98c5d287d760b102888856f0a45d81
L.B. Deyo <lbdeyo@gmail.com>
2014-03-18 11:34:50 -0500

    Pushing changes to main.js

--- motorola1/js/main.js
+++ motorola1/js/main.js
@@ -4,23 +4,23 @@ var bottomAnchor, scrollMax = 2100;
 // Only run this on desktops or tablets
 if (window.matchMedia("(min-width: 600px)").matches) {
 
-	$(window).scroll(function() {
+    $(window).scroll(function() {
 
-		if( $(window).scrollTop() + $(window).height() > $(document).height() - 80) {
-			$('.navbar-fixed-bottom').addClass('on');
-		}
-		else if( $('.navbar-fixed-bottom').hasClass('on') && ( $(window).scrollTop() + $(window).height() < $(document).height() - 100) ){
-			$('.navbar-fixed-bottom').removeClass('on');
-		}
+        if( $(window).scrollTop() + $(window).height() > $(document).height() - 80) {
+            $('.navbar-fixed-bottom').addClass('on');
+        }
+        else if( $('.navbar-fixed-bottom').hasClass('on') && ( $(window).scrollTop() + $(window).height() < $(document).height() - 100) ){
+            $('.navbar-fixed-bottom').removeClass('on');
+        }
 
-	});
+    });
 }
 
 
 
 $(document).ready(function(){
 
-  //  $('body').scrollspy({ target: '.navbar-fixed-top' })
+    //  $('body').scrollspy({ target: '.navbar-fixed-top' })
     onWindowResize();
     gotoTopNavState();
     $(window).resize(function(e){
@@ -36,7 +36,7 @@ $(document).ready(function(){
 //            // you're at the bottom of the page
 //            gotoBottomNavState();
 //        } else {
-          //  gotoTopNavState();
+            //  gotoTopNavState();
         }
 
 
@@ -45,46 +45,47 @@ $(document).ready(function(){
 
     $('.navLink').click(function(){
 
-     //   $('.navLink').removeClass('active');
+        //   $('.navLink').removeClass('active');
 
         //$(this).addClass('active');
 
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-        $('html,body').animate({
-        scrollTop: (target.offset().top - 55)
-        }, 1000);
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 55)
+                }, 1000);
+
+            }
 
         }
-       
-        }
-     return false;
+        return false;
     });
 
 
     function onWindowResize(){
-        console.log('body id: '+$('body').attr('id'));
-
+        $("body").css("background-color", "#d3d3d3");
         bottomAnchor = document.getElementById('pageBottom').offsetTop;
         $(".bothSocials").show();
         $(".mobileFooter").hide();
-        console.log("bottomAnchor: "+bottomAnchor);
         var windowWidth = $(window).width();
         var containerWidth = $(".indexContainer").width();
-        console.log(getScrollY());
-        console.log("containerWidth: "+ containerWidth);
         var cWidthString = containerWidth + "px";
         $("#vid").css("width", cWidthString);
         TweenMax.set($("#infographicButton"), {scaleX:0.8, scaleY:0.8} );
         TweenMax.set($("#downloadButton"), {scaleX:0.8, scaleY:0.8} );
         TweenMax.set($("#whitePaperButton"), {scaleX:0.8, scaleY:0.8} );
-//        $("#socialIcons").css("margin-left", '-50px');
+        $("#infographicButton").css("padding-left", "20px");
+        $("#infographicButton").css("margin-left", "-55px");
+        $("#downloadButton").css("padding-left", "0px");
+        $("#downloadButton").css("margin-left", "0px");
+        TweenMax.set($("#landingNotebook"), {scaleX: 1, scaleY:  1});
+        $("#landingNotebook").css("padding", "0px");
+        $("#landingNotebook").css("margin-left", "10px");
+        $("#landingNotebook").css("margin-top", "25px");
+        $("#landingNotebook").css("margin-bottom", "25px");
 
-//        TweenMax.set($("#socialIcons"), {scaleX: 1, scaleY:1});
-//        TweenMax.set($(".chatCallDiv"), {scaleX:1, scaleY:1} );
-//        $("#socialIcons").css("margin-left", '0px');
         $("#chatPhone").css("margin-left", '0px');
 
 
@@ -114,12 +115,10 @@ $(document).ready(function(){
             $("body").css("background-color", "#292929");
             $(".bothSocials").hide();
             $(".mobileFooter").show();
-//            $("#infographicButton")
             TweenMax.set($("#infographicButton"), {scaleX:0.6, scaleY:0.6} );
             $("#infographicButton").css("padding-left", "0px");
             $("#infographicButton").css("margin-left", "-85px");
-//            $("#infographicButton").css("width", "200px");
-//            $("infographicButton").css("font-size", "0.25em");
+
             TweenMax.set($("#downloadButton"), {scaleX:0.6, scaleY:0.6} );
             $("#downloadButton").css("padding-left", "0px");
             $("#downloadButton").css("margin-left", "-85px");
@@ -131,8 +130,6 @@ $(document).ready(function(){
             $("#landingNotebook").css("margin-bottom", "-80px");
 
 
-//            TweenMax.set($(".chatCallDiv"), {scaleX:0.65, scaleY:0.65} );
-//            $("#socialIcons").css("margin-left", '-50px');
             $("#chatPhone").css("margin-left", '-50px');
             $(".formTable").css("width", "300px");
 
@@ -143,9 +140,7 @@ $(document).ready(function(){
             $(".vr").hide();
 
         }
-//        console.log($("#socialIcons").css("margin-left"));
 
-//        $("#index").css("overflow", "auto");
     }
 
     function getScrollY() {
@@ -155,18 +150,15 @@ $(document).ready(function(){
     function gotoBottomNavState(){
         console.log("go to bottom nav state");
         $("#topNavLogo").hide();
-       $(".navBottomRow").show();
+        $(".navBottomRow").show();
         $(".legal").show();
-      //  $(".hrRow").show();
+        //  $(".hrRow").show();
 
     }
 
     function gotoTopNavState(){
         console.log("go to top nav state");
         $("#topNavLogo").show();
-//        $(".navBottomRow").hide();
-//        $(".legal").hide();
 
-        //  $(".hrRow").hide();
     }
 });
